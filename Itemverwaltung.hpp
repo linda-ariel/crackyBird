ce9a31375f6caf6a2ddc052a1849361c0257b045
hannah-2000 <77071413+hannah-2000@users.noreply.github.com>
2021-01-21 15:11:23 +0100

    Add files via upload
    Itemverwaltung kommentiert

--- Itemverwaltung.hpp
+++ Itemverwaltung.hpp
@@ -45,12 +45,12 @@ class Itemverwaltung {
     
         //füge Item dem Spielfeld hinzu
         void addItem(int hoehe, int art) {
+            //je nachdem welche Höhe (0-2) angegeben wird, werden Iems platziert
             
             //stein
             if (art == 0) {
                 
                 int ypos = 440 - hoehe * 150;
-                //int ypos = 400 - hoehe * 100 - rand() % 100 ;
                 item = new Item(700, ypos, 80, 80, 0, spielfeld);
                 items.push_back(item);
                 
@@ -58,7 +58,6 @@ class Itemverwaltung {
             } else if (art == 1) {
                 
                 int ypos = 440 - hoehe * 120;
-                //int ypos = 400 - hoehe * 100 - rand() % 100 ;
                 item = new Item(700, ypos, 80, 80, 1, spielfeld);
                 items.push_back(item); 
                 
@@ -89,12 +88,7 @@ class Itemverwaltung {
                 
                 //lösche Item aus SVG und Liste, wenn es nicht mehr zu sehen ist
                 if (neuX <= -30) {
-                    /*if (item->getArt() == 0) {
-                        item->löscheStein();
-                    } else {
-                        item->löschen();
-                    }*/
-                    
+ 
                     it = items.erase(it);
                     delete (item);
                     
@@ -107,8 +101,9 @@ class Itemverwaltung {
     
         //Speichert Items, die nah an Spieler sind in einer separaten Liste "naheItems"
         void toList(int x, Item* item){
-                                       //damit kein Item mehrmals gespeichert wird
-            if( x >= 80 && x <= 220 && find(naheItems.begin(), naheItems.end(), item) == naheItems.end() ){
+            
+            //damit kein Item mehrmals gespeichert wird
+            if(find(naheItems.begin(), naheItems.end(), item) == naheItems.end() && x >= 80 && x <= 220){
                     naheItems.push_back(item);
             }
 
@@ -122,30 +117,32 @@ class Itemverwaltung {
         
         //Kollisionsüberprüfung
         int kollision(int protagonistY, int protagonistBildwert) {
-
+            //leere Liste beendet Operation
             if (naheItems.empty()) {
                 return 0;
             }
             
+            //geht Liste "nahItems durch und überprüft Kollision
             for (Item *item : naheItems) {
     
                 int abstandX = abs(item->getXpos() - 150);
                 int abstandY = abs(item->getYpos() - protagonistY);
                 
+                //Protagonist Höhe/Breite = 80
                 int grenzeY = 80;
                 int grenzeX = 80;
-                
-               if ( protagonistBildwert == 2 ) {
-                   
+               
+                // wenn Protagonist sich duckt ist Grenze kleiner
+               if (protagonistBildwert == 2) {                   
                    grenzeY = 65;
-                   grenzeX = 65;
-             
+                   grenzeX = 65;            
                }
-
+                
+                // Rückgabewert wird in main verwendet um Punktestand oder Leben zu verändern
                 //  1 = setLeben(+1), 2 = setPunkte(+1), 3 = setPunkte(+3), 0 = keine Kollison
                 // -1 = setLeben(-1), -3 = setPunkte(-3)
                 
-                //mit Münze
+                //*mit Münze*
                 if (item->getArt() == 1) {
 
                     if (abstandX <= grenzeX && abstandY <= grenzeY) {
@@ -154,12 +151,12 @@ class Itemverwaltung {
                         delete (item);
                         return 2;                        
                     }
-
                 }
                     
-                //mit Feind
-                if (item->getArt() == 2) {                    
-                    //wenn Protagonist Feind besiegt
+                //*mit Feind*
+                if (item->getArt() == 2) { 
+                    
+                    //wenn Protagonist Feind besiegt (geht nur im Kampfmodus -> Bildwert 3)
                     if (protagonistBildwert == 3 && abstandX <= grenzeX && abstandY <= grenzeY ) {
                         item->feindTot();
                         items.remove(item);
@@ -172,21 +169,22 @@ class Itemverwaltung {
                     }
                 }
                     
-                //mit Stein
+                //*mit Stein*
                 if (item->getArt() == 0) {
   
                     //wenn Protagonist mit Stein kollidiert
                     if (protagonistBildwert != 3 && abstandX <= grenzeX && abstandY <= grenzeY ) {
                         item->steinKollidiert();
-                        return -3;                                           
-                    //wenn Protagonist Stein "bekämpft"
+                        return -3;  
+                        
+                    //wenn Protagonist Stein "bekämpft" (geht nur im Kampfmodus -> Bildwert 3)
                     } else if (protagonistBildwert == 3 && abstandX <= grenzeX && abstandY <= grenzeY ) {
                         item->steinZerstoert();
-                    cout << "FIGHT" << endl;
                         return 3;
                     }
                 }
-            } return 0;
+            } 
+            return 0;
         }
                
     
